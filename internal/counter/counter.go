0ddd1d6fd00a4d568e03e7f5fea48a2931540a7a
Alexey Galyshev <aleksey.galyshev@myoffice.team>
2020-06-02 12:19:58 +0300

    fix test && cntV2

--- internal/counter/counter.go
+++ internal/counter/counter.go
@@ -9,7 +9,7 @@ func New() *counter {
 func (c *counter) Count(pp []int) int {
 	cp := make([]int, len(pp))
 	copy(cp, pp)
-	return countSlice(cp)
+	return countSliceV2(cp)
 }
 
 func countSlice(pp []int) int {
@@ -27,7 +27,6 @@ func countSlice(pp []int) int {
 	}
 	return cnt
 }
-
 func iterate(pp []int) ([]int, bool) {
 	index := len(pp) - 1
 	for i := len(pp) - 1; i >= 0; i-- {
@@ -55,4 +54,58 @@ func iterate(pp []int) ([]int, bool) {
 		index--
 	}
 	return pp[:stop], false
+}
+
+func countSliceV2(pp []int) int {
+	if len(pp) == 0 {
+		return 0
+	}
+	done := false
+	cnt := 0
+	for {
+		pp, done = iterateV2(pp)
+		cnt++
+		if done {
+			break
+		}
+	}
+	return cnt
+}
+
+func iterateV2(pp []int) ([]int, bool) {
+	if len(pp) == 0 {
+		return []int{}, true
+	}
+	index := len(pp) - 1
+	seatNum := pp[index]
+	stop := index + 1
+	setStop := false
+	for {
+		if index == -1 || seatNum == -1 {
+			return pp[:stop], len(pp[:stop]) == 0
+		}
+		if pp[index] == 0 {
+			index--
+			if !setStop {
+				stop--
+			}
+			continue
+		}
+
+		seat := false
+		if pp[index] <= seatNum {
+			seatNum = pp[index]
+			pp[index] = 0
+			seat = true
+		}
+		seatNum--
+		if !setStop && seat {
+			stop--
+		}
+
+		if !seat && !setStop {
+			setStop = true
+		}
+		index--
+	}
 }
